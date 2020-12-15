7e802fbfa2fb6b308845c8c3cb9eeb882fd423c7
GitHub Action <action@github.com>
2020-12-15 17:05:01 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/chikishevvladislav/featuretemplate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.chikishevvladislav.featuretemplate.services
+
+import com.github.chikishevvladislav.featuretemplate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
